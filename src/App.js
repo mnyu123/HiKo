23056d8be49ba2688f2498f3101c3887a5833387
mnyu123 <whdudwo5582@gmail.com>
2023-10-09 17:01:09 +0900

    첫번째 페이지 커밋

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>안녕!?!-진우</h1>
-      <h2>안녕!!!</h2>
-      <h3>안녕!!!!-승진</h3>
-      <p>아아아아</p>
-    </div>
+    <body>
+      <div className="App">
+        <h1 class="game-title">맞추기 게임</h1>
+        <div class="level-buttons">
+          <button class="level-button" onclick="selectLevel(1)">
+            레벨 1
+          </button>
+          <button class="level-button" onclick="selectLevel(2)">
+            레벨 2
+          </button>
+        </div>
+      </div>
+    </body>
   );
 }
 
