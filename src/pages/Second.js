b5a28fc6aa0771f5d2b7dc00e0bf1b699f2d41ae
mnyu123 <whdudwo5582@gmail.com>
2023-10-11 18:52:55 +0900

    첫페이지 basename 추가 + CSS주석(GPT) + 첫문제 마지막문제 예외처리

--- src/pages/Second.js
+++ src/pages/Second.js
@@ -7,22 +7,40 @@ import CustomProgressBar from "../common/Progressbar";
 export default function Second() {
   console.log("리액트 컴포넌트가 잘 연결되었습니다.");
 
+  // 재할당(변수 내용 다시 바꾸는거)이 불가능한 변수 const 생성
+  // currentQuestion: 현재 문제 번호
+  // setCurrentQuestion: 현재 문제 번호를 변경하는 함수
+  // useState(1): currentQuestion의 초기값을 1로 설정 = 무슨말이냐면 첫번째 문제를 보여주기 위해 1로 설정
   const [currentQuestion, setCurrentQuestion] = useState(1);
 
+  // timeLeft: 남은 시간
+  // setTimeLeft: 남은 시간을 변경하는 함수
+  // useState(300): timeLeft의 초기값을 300으로 설정 = 5분(5 * 60초)
   const [timeLeft, setTimeLeft] = useState(300); // 5분(5 * 60초)
 
+  // progress: ProgressBar 진행률
+  // setProgress: ProgressBar 진행률을 변경하는 함수
+  // useState(0): progress의 초기값을 0으로 설정
   const [progress, setProgress] = useState(0); // ProgressBar 진행률 상태
 
+  // API를 추가하고 나서는 여기가 변수처럼 변해야 할듯
+  // questions: 문제 목록
+  const questions = [
+    { number: "첫 번째", content: "바람" },
+    { number: "두 번째", content: "시나브로" },
+    { number: "세 번째", content: "나무" },
+  ];
 
   useEffect(() => {
     if (timeLeft === 0) {
-      // 시간이 다 끝났을 때 다음 문제로 이동하거나 필요한 작업을 수행합니다.
+      // 시간이 0 이면 다음문제로 넘어감.
       if (currentQuestion < questions.length) {
         setCurrentQuestion(currentQuestion + 1);
         setTimeLeft(300); // 다음 문제로 이동할 때 시간 초기화
       }
     }
 
+    // 1초마다 timeLeft를 1씩 감소시킴
     const timer = setInterval(() => {
       setTimeLeft((prevTime) => {
         if (prevTime === 0) {
@@ -44,34 +62,41 @@ export default function Second() {
     };
   }, [timeLeft, currentQuestion]);
 
-  // API를 추가하고 나서는 여기가 변수처럼 변해야 할듯
-  const questions = [
-    { number: "첫 번째", content: "바람" },
-    { number: "두 번째", content: "두 번째 문제의 내용" },
-    { number: "세 번째", content: "세 번째 문제의 내용" },
-  ];
+  // 만약 문제를 다 풀거나 1번문제 이전으로 돌아가려고 하면
+  const showAlertMessage1 = () => {
+    alert('이것이 첫번째 문제입니다!');
+  };
 
+  // 마지막 문제를 만나고 정답을 체크했을때
+  const showAlertMessage2 = () => {
+    alert('이것이 마지막 문제입니다!');
+  };
+
+  // 다음 문제로 넘어가는 함수
   const nextQuestion = () => {
     if (currentQuestion < questions.length) {
       setCurrentQuestion(currentQuestion + 1);
       setProgress(0); // ProgressBar 초기화
       setTimeLeft(300); // 카운트다운 초기화
     }
+    else{
+      // 마지막 문제입니다 출력 후 다음 컴포넌트로 이동 예정
+      showAlertMessage2();
+    }
   };
 
+  // 이전 문제로 넘어가는 함수
   const prevQuestion = () => {
-    if (currentQuestion <= questions.length) {
+    if (currentQuestion > 1) {
       setCurrentQuestion(currentQuestion - 1);
       setProgress(0); // ProgressBar 초기화
       setTimeLeft(300); // 카운트다운 초기화
     }
+    else{
+      // 첫번째 문제임을 알려줌
+      showAlertMessage1();
+    }
   };
-
-  // const formatTime = (seconds) => {
-  //   const minutes = Math.floor(seconds / 60);
-  //   const remainingSeconds = seconds % 60;
-  //   return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
-  // };
 
   return (
     <body>
@@ -80,15 +105,25 @@ export default function Second() {
       <div className="container">
         <div className="question-container">
           <div className="question-number">{`${
+            // qusetions = 문제 목록
+            // currentQuestion = 현재 문제 번호
+            // 형태 = 1번째 + 문제 => 첫번째 문제 이렇게 된다.
             questions[currentQuestion - 1].number
           } 문제`}</div>
           <div className="question-content">
-            {questions[currentQuestion - 1].content}
+            {/* 문제 낼 단어들도 한개씩 불러서 온다 */}
+            {`${questions[currentQuestion - 1].content} 
+            의 뜻은?`}
           </div>
         </div>
-        <CustomProgressBar now={Math.round(((300 - timeLeft) / 300) * 100)} />{" "}
+        {/* 부트 스트랩 프로그레스 바 사용 | math.round는 정수로 계산이고 */}
+        {/* 프로그레스바 1초씩 증가하는 연산 */}
+        <CustomProgressBar
+          now={Math.round(((300 - timeLeft) / 300) * 100)}
+        />{" "}
         {/* ProgressBar 컴포넌트 사용 */}
-        {/* <div className="time-left">{formatTime(timeLeft)}</div> */}
+        {/* 여기는 정답을 고르는 선택 버튼들 */}
+        {/* API가 도입되면 여기도 변수를 받아들이는 구조로 변경되어야 할것. */}
         <div className="answers">
           <button onClick={nextQuestion} className="btn-answer">
             정답1
