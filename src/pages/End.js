0041149ba57f8ed6faff4ee338a33430c0d84cfe
dntmdwls42 <dntmdwls42@wku.ac.kr>
2023-10-13 18:21:01 +0900

    End.js End.css 제작 완료, 시험지에 쓸 Font 추가, 시험지 텍스쳐 추가

--- src/pages/End.js
+++ src/pages/End.js
@@ -6,11 +6,35 @@ export default function End() {
   console.log("리액트 컴포넌트가 잘 연결되었습니다.");
   return (
     <body>
-      <div class="result-message">게임이 종료되었습니다.</div>
-      <div class="score">
-        당신이 맞춘 개수는 <span id="score">?/10</span> 입니다.
+      <div class="end-container">
+        <div class="testpapername">
+          <span>2023학년도 대학수학능력시험 문제지</span>
+          <br></br>
+          <span class="testtitlearea">상식 영역</span>
+          <div class="namenumber">
+              성명 : <input type="text"></input>
+              수험번호 : <input type="text"></input>
+          </div>
+        </div>
+        <div class="titleendboxcontainer">
+          <br></br>
+        </div>
+        <br></br>
+        <div>
+          <div class="result-message">게임이 종료되었습니다.</div>
+          <div class="score">
+            당신이 맞춘 개수는 <span id="score">?/10</span> 입니다.
+          </div>
+          <br></br>
+        </div>
+        <div>
+          <a href="/HiKo" class="button">메인 화면으로</a>
+          <a href="/HiKo" class="button">다시 풀어보기</a>
+        </div>
       </div>
-      <Link to="/">첫페이지 이동</Link>
     </body>
   );
+}
+{
+  /* <Link to="/">첫페이지 이동</Link> */
 }
