984810f2beb121f82f7c592a781ada2605404300
HyeokJae <gurwo63@naver.com>
2023-10-14 20:53:33 +0900

    팝업창 따로 만들고 헤더 파일에 추가

---
+++ src/common/Popup.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import "../css/popup.css";
+export default function Popup()
+{
+    const [isMouseOver, setIsMouseOver] = useState(false);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+        setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+    }, []);
+
+    const handleMouseEnter = () => {
+    setIsMouseOver(true);
+    };
+    const handleMouseLeave = () => {
+    setIsMouseOver(false);
+    };
+
+    return (  
+    <div
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}>
+        {isMouseOver && (
+          <div
+            style={{
+              top: position.y + 10, // Adjust the position as needed
+              left: position.x + 10, // Adjust the position as needed
+            }}
+          >
+          팝업 내용
+        </div>
+      )}
+        ?</div>
+    );
+}
