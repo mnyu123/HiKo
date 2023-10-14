5e7ee4e47870a9aee6b667d5ebbb3ecd89cd9b89
HyeokJae <gurwo63@naver.com>
2023-10-14 20:55:44 +0900

    헤더 파일에 팝업 요소 추가

--- src/common/header.js
+++ src/common/header.js
@@ -1,28 +1,18 @@
 import { Link } from "react-router-dom";
 import "../css/header.css";
 import Button from "./Button";
-import Modal from "react-modal";
-import React, { useState } from 'react';
+import Popup from "./Popup";  //도움말 표시를 위한 팝업창 따로 생성함
 // 여기 컴포넌트에는 왼쪽 상단으로 프로젝트 이름, ?버튼이 존재할것.
 
 export default function Header() {
 
-  const [modalIsOpen, setModalIsOpen] = useState(false);
   return (
     <header className="mw">
-      
-    <div className="help">
-      <button onMouseEnter={()=> setModalIsOpen(true)}>?</button>
-      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-        여기서 도움말 내용을 작성하시면 됩니다.<br/>
-        <button onClick={() => setModalIsOpen(false)}>닫기</button>
-      </Modal>
-    </div>
     
       <h1>
         히코
       </h1>
-
+        <Popup/>
 
       <nav>
         <Link to="/end">마지막페이지</Link>
