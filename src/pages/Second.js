08307c3d26278af7581ecef0c7f11a946b43c1e9
mnyu123 <whdudwo5582@gmail.com>
2023-10-20 20:22:38 +0900

    안쓰는 요소 삭제 , progress는 삭제금지

--- src/pages/Second.js
+++ src/pages/Second.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import Header from "../common/Header";
 import "../css/second.css"; // second.css 파일을 가져옵니다.
 
@@ -7,11 +7,11 @@ import Timer from "../common/timer";
 
 import TypingTitle from "./TypingTitle";
 
-export default function Second({ data }) {
+export default function Second({data}) {
   console.log("로딩테스트");
 
   // 인수값이 넘어왔는지 콘솔로 테스트
-  // 빈 배열을 전달하여 이펙트가 한 번만 실행되도록 설정
+ // 빈 배열을 전달하여 이펙트가 한 번만 실행되도록 설정
   // 재할당(변수 내용 다시 바꾸는거)이 불가능한 변수 const 생성
   // currentQuestion: 현재 문제 번호
   // setCurrentQuestion: 현재 문제 번호를 변경하는 함수
@@ -21,7 +21,7 @@ export default function Second({ data }) {
   // progress: ProgressBar 진행률
   // setProgress: ProgressBar 진행률을 변경하는 함수
   // useState(0): progress의 초기값을 0으로 설정
-  const [setProgress] = useState(0); // ProgressBar 진행률 상태
+  const [progress, setProgress] = useState(0); // ProgressBar 진행률 상태
 
   // answer: 사용자의 답변
   // setAnswer: 답변을 변경하는 함수
@@ -33,11 +33,7 @@ export default function Second({ data }) {
 
   const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
 
-  const [correctAnswers] = useState([
-    ["바람", "바람의 뜻"],
-    ["시나브로", "시나브로의 뜻"],
-    ["나무", "나무의 뜻"],
-  ]);
+  // const [correctAnswers] = useState([]);
 
   const clearInput = () => {
     setInput("");
@@ -50,7 +46,7 @@ export default function Second({ data }) {
   }, [timeLeft]); // timeLeft 값이 변경될 때만 실행
   // API를 추가하고 나서는 여기가 변수처럼 변해야 할듯
   // questions: 문제 목록
-
+  
   // const questions = [
   //   {
   //     number: "첫 번째",
@@ -59,28 +55,20 @@ export default function Second({ data }) {
   //   { number: "두 번째", content: "모르는 사이에 조금씩 조금씩." },
   //   { number: "세 번째", content: data.definition },
   // ];
-  const questions = [
-    { number: "첫번째" },
-    { number: "두번째" },
-    { number: "세번째" },
-    { number: "네번째" },
-    { number: "다섯번째" },
-    { number: "여섯번째" },
-    { number: "일곱번째" },
-    { number: "여덟번째" },
-    { number: "아홉번째" },
-    { number: "열번째" },
-  ];
-  //데이터리스트 만큼만 for문이 작동됨
-  if (data.length !== 0) {
-    for (let i = 0; i < data.length; i++) {
-      questions[i] = {
-        number: questions[i].number,
-        contents: data[i].definition,
+  const questions= [
+    {number:"첫번쨰"},{number:"두번째"},{number:"세번째"},{number:"네번째"},{number:"다섯번째"},
+    {number:"여섯번째"},{number:"일곱번째"},{number:"여덟번째"},{number:"아홉번째"},{number:"열번째"}
+    ]
+    //데이터리스트 만큼만 for문이 작동됨
+if (data.length!==0) {
+  for(let i = 0; i<data.length; i++){
+    questions[i]={
+     number: questions[i].number,
+      contents: data[i].definition
       };
-    }
-  }
-
+    }  
+}
+    
   // 만약 문제를 다 풀거나 1번문제 이전으로 돌아가려고 하면
   const showAlertMessage1 = () => {
     alert("이것이 첫번째 문제입니다!");
@@ -147,8 +135,8 @@ export default function Second({ data }) {
     // 현재는 단순히 콘솔에 답변을 출력하는 예시입니다.
     // console.log("사용자의 답변:", answer);
 
-    // const currentAnswer = questions[currentQuestion - 1].content;
-    const possibleAnswers = correctAnswers[currentQuestion - 1];
+    // const currentAnswer = data[currentQuestion - 1].word;
+    const possibleAnswers = data[currentQuestion - 1].word;
 
     if (possibleAnswers.includes(answer)) {
       setIsAnswerCorrect(true);
@@ -194,15 +182,15 @@ export default function Second({ data }) {
             // 형태 = 1번째 + 문제 => 첫번째 문제 이렇게 된다.
             questions[currentQuestion - 1].number
           } 문제`}</div>
-          <div className="question-content">
-            {questions[currentQuestion - 1].contents ? (
-              <TypingTitle
-                content={questions[currentQuestion - 1].contents}
-                input={input}
-              />
-            ) : (
-              <div>로딩중</div>
-            )}
+          <div className="question-content">{
+            questions[currentQuestion - 1].contents?
+      <TypingTitle
+      content={questions[currentQuestion - 1].contents}
+      input={input}
+      />:<div>로딩중</div>
+
+          }
+             
             의 뜻은?
           </div>
         </div>
