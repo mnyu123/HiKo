3f8825fa8394d3402a8f11dc638029f3560fb346
dntmdwls42 <dntmdwls42@wku.ac.kr>
2023-10-21 12:06:01 +0900

    End.CSS에 이름-> 레벨 변경

--- src/pages/End.js
+++ src/pages/End.js
@@ -3,9 +3,29 @@ import "../css/end.css";
 import { Link, useLocation } from "react-router-dom";
 
 export default function End() {
+
+  const userrank = [
+    { number: "MZ세대" },
+    { number: "국어선생님" },
+    { number: "조선시대 선비" },
+  ];
+
+  const lol = userrank[2].number
+
   const location = useLocation();
 
   console.log("리액트 컴포넌트가 잘 연결되었습니다.",location.state);
+
+  if(location.state <= 3) {
+    const mz = userrank[0].number
+    console.log("테스트1",mz);
+  } else if(location.state >= 7)
+  {
+    const teacherlevel = userrank[1].number
+    console.log("와..국어능력보소..",teacherlevel);
+  } else {
+    console.log("누구세요",lol);
+  }
 
   return (
     <body>
@@ -16,8 +36,8 @@ export default function End() {
           <div class="testtitlearea">속담 영역</div>
           </div>
           <div class="namenumber">
-            <div class="studentname">
-              성명 : <input type="text"></input>
+              <div class="studentname">
+              등급: <div class="text">{lol}</div>
               </div>
               <div class="studentnumber">
               수험번호 : <input type="text" class="partnum2"></input>
@@ -37,7 +57,7 @@ export default function End() {
         </div>
         <div>
           <a href="/HiKo" class="endpage-button">메인 화면으로</a>
-          <a href="/HiKo" class="endpage-button">다시 풀어보기</a>
+          <a href="/HiKo/second" class="endpage-button">다시 풀어보기</a>
         </div>
       </div>
     </body>
